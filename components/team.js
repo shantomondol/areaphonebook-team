a5cfe719d3c25dfb0edab1322187e7866a9a0167
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-02-16 16:26:49 +0600

    text change

--- components/team.js
+++ components/team.js
@@ -16,12 +16,12 @@ function teamTemplate(team) {
         </div>
     </div>
 
-    <!-- Founder & Co-Founder -->
+    <!-- Founder & Founder -->
     <div class="col-md-4">
         <div class="justify-content-between align-content-between team-card icon-card-highlight">
             <a href="cdn/membar.png"><img src="cdn/membar.png" alt="sharif imtaz" class="team-img"></a>
             <h2>Sharif Imtaz</h2>
-            <p class="text-center">Co-Founder, CMO</p>
+            <p class="text-center">Founder, CMO</p>
             <a href="tel:+08801943054316" target="_blank"><i class="fas fa-phone-square-alt"></i></a>
             <a href="https://www.facebook.com/SharifImtaz" target="_blank"><i class="fab fa-facebook-square"></i></a>
             <a href="mailto:shorifallrobi321@gmail.com" target="_blank"><i class="fas fa-envelope"></i></a>
